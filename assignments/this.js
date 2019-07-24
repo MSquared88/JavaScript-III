7bd8163db3eaabc0c5225180c826929f4c2055e9
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-07-24 17:19:26 -0500

    finished prototypes

--- assignments/this.js
+++ assignments/this.js
@@ -1,7 +1,8 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. The window or global scope is where everything in JavaScript exists. So you could say the window is everything that  JavaScript can access
+* 1. The window is the browser representaion of the  global scope and it is where everything in JavaScript exists. So you could say the window is everything that  JavaScript can access 
+when we console.log(this) we are give back a object that is full of other objects and functions that can be accesed at anytime.
 
 * 2. Implicit binding basically means that when he call a function using dot notation on an object the this in that function refers to whatever is to the left of the dot notation is implied to be (this).
 
@@ -9,7 +10,7 @@
 
 * 4. explicit binding lets us use a call a function on an object using one of the built in JavaScript methods wich is .call()
 
-.call() takes in one argument. and that argument is the object we want to call a function on. If we are using the this keyword in that function we explicitly stating what (this) means by passing an object through the function.
+.call() takes in one argument. and that argument is the object we want to call a function on. If we are using the this keyword in that function we are explicitly stating what (this) refers to  by passing an object through the function.
 
 *
 * write out a code example of each explanation above
